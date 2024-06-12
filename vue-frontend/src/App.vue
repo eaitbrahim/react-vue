88c3348ce780ca1dbc144147de6a230355f2f547
eaitbrahim <eaitbrahim@gmail.com>
2024-06-12 10:45:19 -0400

    Clean up vue app
    Co-authored-by: Harsha Saddanathan <hsaddanathan21@gmail.com>

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,16 +1,7 @@
 <script setup>
-  import { defineComponent } from 'vue';
   import Header from './components/header/Header.vue';
   import Banner from './components/banner/Banner.vue';
   import Main from './components/Main.vue';
-  
-  export default defineComponent({
-    components: {
-      Header,
-      Banner,
-      Main
-    },
-  });
 </script>
   
 <template>
